4221e497bc3ad2294bd3f4119782af450b9c589b
bradleyyin <bradleyyin@gmail.com>
2019-11-08 22:35:25 -0800

    project created

---
+++ BestPlaceToLive/BestPlaceToLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestPlaceToLive
+//
+//  Created by Bradley Yin on 11/8/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

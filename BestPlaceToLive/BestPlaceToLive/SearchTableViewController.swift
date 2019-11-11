f180adda29cd57cf362efab00a7d3b42cd524c1b
luqmaan <luq.j.khan@gmail.com>
2019-11-11 13:15:25 -0600

    Add search tableview controller subclass file

---
+++ BestPlaceToLive/BestPlaceToLive/SearchTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchTableViewController.swift
+//  BestPlaceToLive
+//
+//  Created by Luqmaan Khan on 11/11/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}

310933b1ce09622b539f798cb7a015ee70f8f0d7
Jeffrey <SantanaJny@gmail.com>
2019-11-11 22:07:18 -0500

    Creating signup/login storyboard

---
+++ BestPlaceToLive/BestPlaceToLive/View Controllers/LoginVC.swift
@@ -0,0 +1,37 @@
+//
+//  LoginVC.swift
+//  BestPlaceToLive
+//
+//  Created by Jeffrey Santana on 11/11/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+	// MARK: - IBOutlets
+	
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	// MARK: - Properties
+	
+	
+	// MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	// MARK: - IBActions
+	
+	@IBAction func loginBtnTapped(_ sender: Any) {
+	}
+	
+	// MARK: - Helpers
+	
+	
+}
---
+++ BestPlaceToLive/BestPlaceToLive/View Controllers/SignupVC.swift
@@ -0,0 +1,37 @@
+//
+//  SignupVC.swift
+//  BestPlaceToLive
+//
+//  Created by Jeffrey Santana on 11/11/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+	// MARK: IBOutlets
+	
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	// MARK: Properties
+	
+	
+	// MARK: Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	// MARK: IBActions
+	
+	@IBAction func signupBtnTapped(_ sender: Any) {
+	}
+	
+	// MARK: Helpers
+
+}

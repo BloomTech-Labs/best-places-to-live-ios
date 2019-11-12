62d215c011a43bc3fd60f80db2ece64ca861aa84
Bradley <bradleyyin@gmail.com>
2019-11-11 17:52:53 -0800

    add explore view controller

---
+++ BestPlaceToLive/ExploreViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExploreViewController.swift
+//  BestPlaceToLive
+//
+//  Created by Bradley Yin on 11/11/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

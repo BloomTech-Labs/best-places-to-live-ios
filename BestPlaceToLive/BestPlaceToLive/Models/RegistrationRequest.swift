0c1f4b92a1066216d68c368f7bbfb0ff018a3689
Jeffrey <SantanaJny@gmail.com>
2019-11-14 14:26:46 -0500

    Refactoring login/register network calls

---
+++ BestPlaceToLive/BestPlaceToLive/Models/RegistrationRequest.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationRequest.swift
+//  BestPlaceToLive
+//
+//  Created by Jeffrey Santana on 11/14/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
+
+struct RegistrationRequest: Codable {
+	let name: String
+	let email: String
+	let password: String
+	let location: String
+}

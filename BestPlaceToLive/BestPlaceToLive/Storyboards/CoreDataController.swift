c5a529adab61692a8e6bd503120ec46be1c699a3
luqmaan <luq.j.khan@gmail.com>
2019-11-15 12:30:24 -0600

    Add crud operations to core data controller

--- BestPlaceToLive/BestPlaceToLive/Storyboards/CoreDataController.swift
+++ BestPlaceToLive/BestPlaceToLive/Storyboards/CoreDataController.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class CoreDataController {
+    
+    func saveToPersistentStore() {
+        let moc = CoreDataStack.shared.mainContext
+        do {
+            try moc.save()
+        }
+        catch {
+            NSLog("Error saving to persistent store: \(error)")
+            moc.reset()
+        }
+    }
+    
+    func addCitySearch(id: UUID, cityName: String, filters: NSArray? = nil, cityPhoto: String? = nil) {
+        _ = CitySearch(id: id, cityName: cityName, cityPhoto: cityPhoto ?? "", filters: filters ?? NSArray())
+        saveToPersistentStore()
+    }
+    
+    func deleteCitySearch(citySearch: CitySearch) {
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(citySearch)
+        saveToPersistentStore()
+    }
+    
+    func updateCitySearch(citySearch: CitySearch, cityName: String, filters: NSArray? = nil, cityPhoto: String? = nil) {
+        citySearch.cityName = cityName
+        citySearch.filters = filters
+        citySearch.cityPhoto = cityPhoto
+        saveToPersistentStore()
+    }
+    
+}
+

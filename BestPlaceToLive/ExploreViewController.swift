ccd859239ec9eff57d0868732a8b4bb4810f8913
Bradley <bradleyyin@gmail.com>
2019-11-19 12:47:03 -0800

    connect to detail page

--- BestPlaceToLive/ExploreViewController.swift
+++ BestPlaceToLive/ExploreViewController.swift
@@ -69,15 +69,16 @@ class ExploreViewController: UIViewController {
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "TopTenToDetailSegue" {
+//            guard let detailVC = segue.destination as? 
+//        }
+//    }
+    
 
 }
 extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {

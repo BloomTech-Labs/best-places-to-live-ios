e7378e9e8b08f51fdad9041c359959cf508d10ab
Bradley <bradleyyin@gmail.com>
2019-11-20 15:53:42 -0800

    update testing

--- BestPlaceToLive/BestPlaceToLiveTests/CoreDataTests.swift
+++ BestPlaceToLive/BestPlaceToLiveTests/CoreDataTests.swift
@@ -22,20 +22,20 @@ class CoreDataTests: XCTestCase {
     
     func testAddSearchAndFetchSingleSearchFromPersistentStore() {
         let id = UUID().uuidString
-        controller.addCitySearch(id: id, cityName: "Name", filters: [""], cityPhoto: "")
+        controller.addCitySearch(id: id, cityName: "Name", cityPhoto: "", group: Group(name: "test"))
         let addedSearch = controller.fetchSingleCitySearchFromPersistence(identifier: id, context: CoreDataStack.shared.mainContext)
         XCTAssertEqual("Name", addedSearch?.cityName )
     }
-    
+
     func testUpdateSearch() {
-        let newSearch = CitySearch(id: UUID().uuidString, cityName: "New York", cityPhoto: "", filters: [""])
+        let newSearch = CitySearch(id: UUID().uuidString, cityName: "New York", cityPhoto: "", parentGroup: Group(name: "test"))
         controller.updateCitySearch(citySearch: newSearch, cityName: "Palo Alto", filters: [""])
         XCTAssertEqual("Palo Alto", newSearch.cityName)
     }
-    
+
     func testDeleteSearch() {
         let id = UUID().uuidString
-        let newSearch = CitySearch(id: id, cityName: "New York", cityPhoto: "", filters: [""])
+        let newSearch = CitySearch(id: UUID().uuidString, cityName: "New York", cityPhoto: "", parentGroup: Group(name: "test"))
         controller.deleteCitySearch(citySearch: newSearch)
         XCTAssertNil(controller.fetchSingleCitySearchFromPersistence(identifier: id, context: CoreDataStack.shared.mainContext))
     }

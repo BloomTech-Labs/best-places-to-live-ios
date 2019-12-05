1e488c729332e5aa874d0a591959f971752bd590
Jeffrey <SantanaJny@gmail.com>
2019-12-05 15:34:28 -0500

    Adding new network calls

---
+++ BestPlaceToLive/BestPlaceToLive/Models/Requests/CityLikeRequest.swift
@@ -0,0 +1,9 @@
+//
+//  CityLikeRequest.swift
+//  BestPlaceToLive
+//
+//  Created by Jeffrey Santana on 12/5/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
---
+++ BestPlaceToLive/BestPlaceToLive/Models/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  BestPlaceToLive
+//
+//  Created by Jeffrey Santana on 12/5/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
